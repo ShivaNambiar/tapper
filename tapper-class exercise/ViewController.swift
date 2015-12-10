bac9c648968d626ed85c746e1b98af67186a64d6
ShivaNambiar <shinam@shis-macbook-pro.local>
2015-12-10 11:59:42 -0500

    Initial Commit

---
+++ tapper-class exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tapper-class exercise
+//
+//  Created by Shi Nam on 2015-12-10.
+//  Copyright © 2015 Shi Nam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
